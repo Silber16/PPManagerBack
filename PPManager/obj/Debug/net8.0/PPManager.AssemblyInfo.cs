84e2c0ce610f09362562fb5d3f9bad8683447248
Francisco Silberberg <fransilber16@gmail.com>
2024-10-22 10:59:29 -0300

    adding cors perm

--- PPManager/obj/Debug/net8.0/PPManager.AssemblyInfo.cs
+++ PPManager/obj/Debug/net8.0/PPManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f609897e248a481b61c3d00afb33b679f730e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30d4ccf14664a8e61e5406c959d88c28155a9d9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
